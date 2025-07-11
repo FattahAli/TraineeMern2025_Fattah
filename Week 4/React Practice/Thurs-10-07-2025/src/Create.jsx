efdc8371fff431557687f8ebdc9e213712f8678c
fattahali37 <fattah.ali@buggcy.com>
2025-07-11 14:31:07 +0500

    completed day 2

--- Week 4/React Practice/Thurs-10-07-2025/src/Create.jsx
+++ Week 4/React Practice/Thurs-10-07-2025/src/Create.jsx
@@ -1,13 +1,47 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    console.log(title, body, author);
+    fetch("http://localhost:8000/blogs",
+      {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify(blog),
+      }).then (()=>{
+        console.log('new blog added')
+      })
+  };
+
   return (
     <div className="create flex flex-col items-center justify-center gap-y-10">
       <h2 className="text-red-600 font-bold text-2xl">Add a new Blog</h2>
-      <form className="flex flex-col gap-y-3">
+      <form className="flex flex-col gap-y-3" onSubmit={handleSubmit}>
         <label>Blog Title:</label>
-        <input type="text" className="border-gray-500 border" required />
+        <input
+          type="text"
+          className="border-gray-500 border"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog Body:</label>
-        <textarea required  className="border-gray-500 border"></textarea>
-        <select className="border-gray-500 border">
+        <textarea
+          required
+          className="border-gray-500 border"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <select
+          className="border-gray-500 border"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
